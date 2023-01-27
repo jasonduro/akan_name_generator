1e5e5007af0d4a25ac2a4b96fa8d11fd2b508a0e
Jason <product@jasonlamanque.com>
2023-01-27 12:13:35 +0100

    updates to js
    connecting birthday input to js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Get the modal
+/* // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -23,3 +23,25 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+ */
+
+//function 
+
+
+
+function generateAkanName() {
+  let birthDate = document.getElementById("birthday").value
+  let gender = document.getElementById("gender").value
+  var dayIndex = new Date(birthDate).getDay
+
+  //This one evaluates for the day of the week based on the day index
+  const getDayName = (dayIndex) =>{
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[dayIndex];
+  }
+  const dayName = getDayName(dayIndex)
+  console.log(dayName)
+}
+
+
+//https://github.com/apprenticecloud/Akan-1/blob/master/akan.js
